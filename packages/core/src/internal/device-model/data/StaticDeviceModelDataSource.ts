815d481753fc2c21332228210ccb28de434ac8fd
jdabbech-ledger <jalil.dabbech@ledger.com>
2024-08-30 18:18:47 +0200

    :recycle: (core): Add bluetooth models to device model data source

--- packages/core/src/internal/device-model/data/StaticDeviceModelDataSource.ts
+++ packages/core/src/internal/device-model/data/StaticDeviceModelDataSource.ts
@@ -2,6 +2,7 @@ import { injectable } from "inversify";
 
 import { DeviceModelId } from "@api/device/DeviceModel";
 import { InternalDeviceModel } from "@internal/device-model/model/DeviceModel";
+import { BleDeviceInfos } from "@internal/transport/ble/model/BleDeviceInfos";
 
 import { DeviceModelDataSource } from "./DeviceModelDataSource";
 
@@ -10,79 +11,78 @@ import { DeviceModelDataSource } from "./DeviceModelDataSource";
  */
 @injectable()
 export class StaticDeviceModelDataSource implements DeviceModelDataSource {
-  private static deviceModelByIds: {
-    [key in DeviceModelId]: InternalDeviceModel;
-  } = {
-    [DeviceModelId.NANO_S]: new InternalDeviceModel({
-      id: DeviceModelId.NANO_S,
-      productName: "Ledger Nano S",
-      usbProductId: 0x10,
-      legacyUsbProductId: 0x0001,
-      usbOnly: true,
-      memorySize: 320 * 1024,
-      masks: [0x31100000],
-    }),
-    [DeviceModelId.NANO_SP]: new InternalDeviceModel({
-      id: DeviceModelId.NANO_SP,
-      productName: "Ledger Nano S Plus",
-      usbProductId: 0x50,
-      legacyUsbProductId: 0x0005,
-      usbOnly: true,
-      memorySize: 1533 * 1024,
-      masks: [0x33100000],
-    }),
-    [DeviceModelId.NANO_X]: new InternalDeviceModel({
-      id: DeviceModelId.NANO_X,
-      productName: "Ledger Nano X",
-      usbProductId: 0x40,
-      legacyUsbProductId: 0x0004,
-      usbOnly: false,
-      memorySize: 2 * 1024 * 1024,
-      masks: [0x33000000],
-      bluetoothSpec: [
-        {
-          serviceUuid: "13d63400-2c97-0004-0000-4c6564676572",
-          notifyUuid: "13d63400-2c97-0004-0001-4c6564676572",
-          writeUuid: "13d63400-2c97-0004-0002-4c6564676572",
-          writeCmdUuid: "13d63400-2c97-0004-0003-4c6564676572",
-        },
-      ],
-    }),
-    [DeviceModelId.STAX]: new InternalDeviceModel({
-      id: DeviceModelId.STAX,
-      productName: "Ledger Stax",
-      usbProductId: 0x60,
-      legacyUsbProductId: 0x0006,
-      usbOnly: false,
-      memorySize: 1533 * 1024,
-      masks: [0x33200000],
-      bluetoothSpec: [
-        {
-          serviceUuid: "13d63400-2c97-6004-0000-4c6564676572",
-          notifyUuid: "13d63400-2c97-6004-0001-4c6564676572",
-          writeUuid: "13d63400-2c97-6004-0002-4c6564676572",
-          writeCmdUuid: "13d63400-2c97-6004-0003-4c6564676572",
-        },
-      ],
-    }),
-    [DeviceModelId.FLEX]: new InternalDeviceModel({
-      id: DeviceModelId.FLEX,
-      productName: "Ledger Flex",
-      usbProductId: 0x70,
-      legacyUsbProductId: 0x0007,
-      usbOnly: false,
-      memorySize: 1533 * 1024,
-      masks: [0x33300000],
-      bluetoothSpec: [
-        {
-          serviceUuid: "13d63400-2c97-3004-0000-4c6564676572",
-          notifyUuid: "13d63400-2c97-3004-0001-4c6564676572",
-          writeUuid: "13d63400-2c97-3004-0002-4c6564676572",
-          writeCmdUuid: "13d63400-2c97-3004-0003-4c6564676572",
-        },
-      ],
-    }),
-  };
+  private static deviceModelByIds: Record<DeviceModelId, InternalDeviceModel> =
+    {
+      [DeviceModelId.NANO_S]: new InternalDeviceModel({
+        id: DeviceModelId.NANO_S,
+        productName: "Ledger Nano S",
+        usbProductId: 0x10,
+        legacyUsbProductId: 0x0001,
+        usbOnly: true,
+        memorySize: 320 * 1024,
+        masks: [0x31100000],
+      }),
+      [DeviceModelId.NANO_SP]: new InternalDeviceModel({
+        id: DeviceModelId.NANO_SP,
+        productName: "Ledger Nano S Plus",
+        usbProductId: 0x50,
+        legacyUsbProductId: 0x0005,
+        usbOnly: true,
+        memorySize: 1533 * 1024,
+        masks: [0x33100000],
+      }),
+      [DeviceModelId.NANO_X]: new InternalDeviceModel({
+        id: DeviceModelId.NANO_X,
+        productName: "Ledger Nano X",
+        usbProductId: 0x40,
+        legacyUsbProductId: 0x0004,
+        usbOnly: false,
+        memorySize: 2 * 1024 * 1024,
+        masks: [0x33000000],
+        bluetoothSpec: [
+          {
+            serviceUuid: "13d63400-2c97-0004-0000-4c6564676572",
+            notifyUuid: "13d63400-2c97-0004-0001-4c6564676572",
+            writeUuid: "13d63400-2c97-0004-0002-4c6564676572",
+            writeCmdUuid: "13d63400-2c97-0004-0003-4c6564676572",
+          },
+        ],
+      }),
+      [DeviceModelId.STAX]: new InternalDeviceModel({
+        id: DeviceModelId.STAX,
+        productName: "Ledger Stax",
+        usbProductId: 0x60,
+        legacyUsbProductId: 0x0006,
+        usbOnly: false,
+        memorySize: 1533 * 1024,
+        masks: [0x33200000],
+        bluetoothSpec: [
+          {
+            serviceUuid: "13d63400-2c97-6004-0000-4c6564676572",
+            notifyUuid: "13d63400-2c97-6004-0001-4c6564676572",
+            writeUuid: "13d63400-2c97-6004-0002-4c6564676572",
+            writeCmdUuid: "13d63400-2c97-6004-0003-4c6564676572",
+          },
+        ],
+      }),
+      [DeviceModelId.FLEX]: new InternalDeviceModel({
+        id: DeviceModelId.FLEX,
+        productName: "Ledger Flex",
+        usbProductId: 0x70,
+        legacyUsbProductId: 0x0007,
+        usbOnly: false,
+        memorySize: 1533 * 1024,
+        masks: [0x33300000],
+        bluetoothSpec: [
+          {
+            serviceUuid: "13d63400-2c97-3004-0000-4c6564676572",
+            notifyUuid: "13d63400-2c97-3004-0001-4c6564676572",
+            writeUuid: "13d63400-2c97-3004-0002-4c6564676572",
+            writeCmdUuid: "13d63400-2c97-3004-0003-4c6564676572",
+          },
+        ],
+      }),
+    };
 
   getAllDeviceModels(): InternalDeviceModel[] {
     return Object.values(StaticDeviceModelDataSource.deviceModelByIds);
@@ -103,5 +103,42 @@ export class StaticDeviceModelDataSource implements DeviceModelDataSource {
         return deviceModel[key as keyof InternalDeviceModel] === value;
       });
     });
+  }
+
+  getBluetoothServicesInfos(): Record<string, BleDeviceInfos> {
+    return Object.values(StaticDeviceModelDataSource.deviceModelByIds).reduce<
+      Record<string, BleDeviceInfos>
+    >((acc, deviceModel) => {
+      const { bluetoothSpec } = deviceModel;
+      if (bluetoothSpec) {
+        return {
+          ...acc,
+          ...bluetoothSpec.reduce<Record<string, BleDeviceInfos>>(
+            (serviceToModel, bleSpec) => ({
+              ...serviceToModel,
+              [bleSpec.serviceUuid]: {
+                deviceModel,
+                ...bleSpec,
+              },
+            }),
+            {},
+          ),
+        };
+      }
+      return acc;
+    }, {});
+  }
+
+  getBluetoothServices(): string[] {
+    return Object.values(StaticDeviceModelDataSource.deviceModelByIds).reduce<
+      string[]
+    >((acc, deviceModel) => {
+      const { bluetoothSpec } = deviceModel;
+
+      if (bluetoothSpec) {
+        return acc.concat(bluetoothSpec.map((spec) => spec.serviceUuid));
+      }
+      return acc;
+    }, []);
   }
 }
